1ae09967d2a268493d92af8b4f2b5898a25c8e0f
AlejandroChing11 <alejandroching2004@hotmail.com>
2024-04-29 17:07:52 -0500

    [Generando Los modulos]

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ProductModule } from './product/product.module';
+import { CategoryModule } from './category/category.module';
+import { PurchaseModule } from './purchase/purchase.module';
+import { SaleModule } from './sale/sale.module';
+import { SupplierModule } from './supplier/supplier.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, ProductModule, CategoryModule, PurchaseModule, SaleModule, SupplierModule],
   controllers: [AppController],
   providers: [AppService],
 })
